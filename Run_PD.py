f5b909c6fa38759ddf95ed49af427c3ca6fb03ba
fernando <fernando.monticeli@cern.ch>
2025-12-24 10:36:18 -0300

    Updated logic to ignore PSD files

--- Run_PD.py
+++ Run_PD.py
@@ -5,31 +5,37 @@ from pathlib import Path
 import argparse, sys
 
 def main(argv):
-    parser = argparse.ArgumentParser("FreeMAPS - the Free/Libre RIMAPS analysis tool, PDS tool")
-    parser.add_argument('-F', '--FileNames',  required=False, nargs ='+', help="Input file names. I will do PDS on each file listed here.")
+    parser = argparse.ArgumentParser("FreeMAPS - the Free/Libre RIMAPS analysis tool, PSD tool")
+    parser.add_argument('-F', '--FileNames',  required=False, nargs ='+', help="Input file names. I will do PSD on each file listed here.")
     parser.add_argument('-P', '--Path',  required=False, help="Input folder where images live. I will loop over all images in this folder")
     parser.add_argument('-p', '--Plot',  required=False, action="store_true",  help="Plot PSD for each image file")
+    parser.add_argument('-L', '--LogLevel',  required=False, type=int,  help="LogLevel. 1 ERROR, 2 WARNING, 4 INFO, 5 DEBUG, 7 VERBOSE", default = 4)
     args = parser.parse_args()
 
     print()
    
     m_files = []
     if args.Path:
+        print(f'Appending files from path:  {args.Path}')
         for m_file in Path(args.Path).iterdir():
-            #print(f'Processing file from path:  {m_file}')
-            m_files.append(m_file)
+            m_files.append(str(m_file))
 
-    for m_file in args.FileNames:
-        #print(f'Processing file:  {m_file}')
-        m_files.append(m_file)
+    if args.FileNames:
+        print(f'Appending  files  {args.FileNames}')
+        for m_file in args.FileNames:
+            m_files.append(m_file)
 
     print(f'Processing files: {m_files}')
     f = PowerDist.Powerdist()
-    f.LogLevel = 3
+    f.LogLevel = args.LogLevel
 
     for m_file in m_files:
-        f.INFO(f'Computing PDS on {m_file}')
-        m_outputfig = '/'.join(m_file.split('/')[:-1] + ['PDS_'+m_file.split('/')[-1]])
+        if not m_file.split('/')[-1].startswith('PSD'):
+            f.INFO(f'Computing PSD on {m_file}')
+        else:
+            f.WARNING(f'Ignoring PSD produced file {m_file}')
+            continue
+        m_outputfig = '/'.join(m_file.split('/')[:-1] + ['PSD_'+m_file.split('/')[-1]])
 
         f.AddImageFromFile(m_file)
         f.Get2DFFT()
